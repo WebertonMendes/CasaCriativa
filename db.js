024b7a608f3d03a9a5fa125508de503577bf09a4
Weberton Mendes <weberton.mendes@hotmail.com>
2020-04-03 00:07:49 -0300

    CasaCriativa

---
+++ db.js
@@ -0,0 +1,55 @@
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('./casaCriativa.db')
+
+db.serialize(function() {
+  db.run(`
+    CREATE TABLE IF NOT EXISTS ideas(
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      image TEXT,
+      title TEXT,
+      category TEXT,
+      description TEXT,
+      link TEXT
+    );
+  `)
+/*
+  const query = `
+  INSERT INTO ideas(
+    image,
+    title,
+    category,
+    description,
+    link
+  ) VALUES (?,?,?,?,?);
+  `
+
+  const values = [
+    "https://image.flaticon.com/icons/svg/2729/2729048.svg",
+    "Recortes",
+    "Criatividade",
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex et consequuntur est doloribus illum fugit voluptatem ab repellat veniam adipisci atque fuga.",
+    "https://google.com"
+  ]
+
+  db.run(query, values, function(err) {
+    if (err) return console.log(err)
+
+    console.log(this)
+  })
+
+  // DELETE ON DATABASE
+  db.run(`DELETE FROM ideas WHERE id = ?`, [3], function(err) {
+    if(err) return console.log(err)
+    console.log("REGISTRO APAGADO COM SUCESSO!", this)
+  })
+
+  // SELECT ON DATABASE
+  db.all(`SELECT * FROM ideas`, function(err, rows) {
+    if (err) return console.log(err)
+
+    console.log(rows)
+  })
+*/
+})
+
+module.exports = db
---
+++ server.js
@@ -0,0 +1,77 @@
+const express = require('express')
+const server = express()
+
+const db = require("./db")
+
+server.use(express.static("public"))
+
+server.use(express.urlencoded({ extended: true }))
+
+const nunjucks = require('nunjucks')
+nunjucks.configure("views", {
+  express: server,
+  noCache: true
+})
+
+server.get("/", function(require, response) {
+  
+  db.all(`SELECT * FROM ideas`, function(err, rows) {
+    if (err) {
+      console.log(err)
+      return response.send("Erro no Banco de Dados.")
+    }
+
+    const reversedIdeas = [...rows].reverse()
+
+    let lastIdeas = []
+    for (let idea of reversedIdeas) {
+      if (lastIdeas.length < 2) {
+        lastIdeas.push(idea)
+      }
+    }
+
+    return response.render("index.html", { ideas: lastIdeas })
+    })
+})
+
+server.get("/ideias", function(require, response) {
+  db.all(`SELECT * FROM ideas`, function(err, rows) {
+    if (err) {
+      console.log(err)
+      return response.send("Erro no Banco de Dados.")
+    }
+
+    const reversedIdeas = [...rows].reverse()
+    return response.render("ideias.html", { ideas: reversedIdeas })
+  })
+})
+
+server.post("/", function(require, response) {
+  const query = `
+    INSERT INTO ideas(
+      image,
+      title,
+      category,
+      description,
+      link
+    ) VALUES (?,?,?,?,?);
+  `
+
+  const values = [
+    require.body.image,
+    require.body.title,
+    require.body.category,
+    require.body.description,
+    require.body.link,
+  ]
+
+  db.run(query, values, function(err) {
+    if (err) {
+      console.log(err)
+      return response.send("Erro no Banco de Dados.")
+    }
+    return response.redirect("/ideias")
+  })
+})
+
+server.listen(3000)
